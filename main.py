9fa2716ba55add09ab3bd0fd2021111d3cc923d4
amirshfn <amirhoseinshafieyoun@gmail.com>
2020-11-23 22:08:46 +0100

    fixing the non numeric bug

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ end_entsoe = (date_time.today() + timedelta(days=1)).strftime('%Y-%m-%d')
 timezone = "Etc/GMT"  # Europe/Berlin OR Etc/GMT
 
 exog_params, renewables_percentage = combine_generation_and_exogenous_data(start, end, end_entsoe, timezone)
-
+renewables_percentage = renewables_percentage.apply(pd.to_numeric)
+exog_params = exog_params.apply(pd.to_numeric)
 data_frequency_per_day = 96
 # sample call
 renewables_percentage.index = pd.date_range(start="{} 00:00:00".format(start), periods=len(renewables_percentage), freq='15Min')
@@ -17,4 +18,4 @@ exog_params.index = pd.date_range(start="{} 00:00:00".format(start), periods=len
 
 forecasts = run_and_save_S_ARIMAX_model(renewables_percentage, data_frequency_per_day, exog_params[:-data_frequency_per_day], exog_params[-data_frequency_per_day:], config=[(4, 1, 4), (2, 0, 2, 4), 'n'])
 
-forecasts = run_and_save_SARIMA_model(renewables_percentage, data_frequency_per_day, config=[(4, 1, 4), (2, 0, 2, 4), 'n'])
+forecasts_2 = run_and_save_SARIMA_model(renewables_percentage, data_frequency_per_day, config=[(4, 1, 4), (2, 0, 2, 4), 'n'])
